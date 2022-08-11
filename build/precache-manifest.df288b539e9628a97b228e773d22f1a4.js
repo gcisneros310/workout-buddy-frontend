77088399a35c54068c208effd0b527d035ada8d9
gcisneros310 <48630000+gcisneros310@users.noreply.github.com>
2022-08-10 21:06:44 -0700

    make it better

---
+++ build/precache-manifest.df288b539e9628a97b228e773d22f1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b39760d30fd1fb239e2",
+    "url": "/workout-buddy-frontend/static/css/main.94560092.chunk.css"
+  },
+  {
+    "revision": "3b39760d30fd1fb239e2",
+    "url": "/workout-buddy-frontend/static/js/main.3b39760d.chunk.js"
+  },
+  {
+    "revision": "3e37eb9787030fe45227",
+    "url": "/workout-buddy-frontend/static/js/1.3e37eb97.chunk.js"
+  },
+  {
+    "revision": "34fd7da90895c3f3f7cb",
+    "url": "/workout-buddy-frontend/static/js/runtime~main.34fd7da9.js"
+  },
+  {
+    "revision": "73b5ca2db5a1b3162f2db6f8edc03b39",
+    "url": "/workout-buddy-frontend/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Workout-Buddy/precache-manifest.ebf7164e37b38fbdca2902e2f54c2e8b.js"
+  "/workout-buddy-frontend/precache-manifest.df288b539e9628a97b228e773d22f1a4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Workout-Buddy/index.html", {
+workbox.routing.registerNavigationRoute("/workout-buddy-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
